3075ca48097f33dffe624a2a820ea0387fb2dca5
Guillermo Cordeiro <gcordeiro@tegnix.com>
2015-05-21 12:36:28 +0200

    Only update the day on utcoffset positives

--- pirate_add_default_time.py
+++ pirate_add_default_time.py
@@ -4,6 +4,12 @@ from taskw import TaskWarrior
 from pytz import timezone, utc
 
 CONFIG = TaskWarrior.load_config()
+if 'dst' in CONFIG['default']:
+    if CONFIG['default']['dst'].lower() in ['true', '1', 'yes']:
+        dst = True
+    else:
+        dst = False
+
 if 'timezone' in CONFIG['default']:
     DEFAULT_ZONE = timezone(CONFIG['default']['timezone'])
 else:
@@ -19,11 +25,17 @@ def is_local_midnight(timestamp):
     return tmp.time() == time(0, 0, 0)
 
 def set_default_time(timestamp):
+    utcoffset = DEFAULT_ZONE.utcoffset(timestamp, is_dst=dst).days * 24 + DEFAULT_ZONE.utcoffset(timestamp, is_dst=dst).seconds / 3600
+
     tmp = timestamp.replace(
         hour=DEFAULT_TIME.hour,
         minute=DEFAULT_TIME.minute,
         second=DEFAULT_TIME.second,
-        ) + timedelta(days=1)
+        )
+
+    if utcoffset > 0:
+        tmp += timedelta(days=1)
+
     return DEFAULT_ZONE.localize(tmp).astimezone(utc)
 
 def hook_default_time(task):
